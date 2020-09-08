da62e2d4879455ea5517775109877047b0c3fe00
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2020-09-08 11:33:14 -0500

    Prototype 2 Player Movement
    Successfully added playey X and Z axis movement with boundaries.

---
+++ Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Benjamin Schuster
+ * Prototype 2
+ * Controls player movement
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//add this script to player
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+    private float xRange = 14;
+    private float zRange = 14;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get keyboard input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Move player according to input
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        //Stop player from moving out of x-axis boundaries
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //Stop player from moving out of z-axis boundaries
+        if (transform.position.z <= 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+    }
+}
