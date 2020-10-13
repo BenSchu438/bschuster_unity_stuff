d63855dd0a962bbee0bef0c2c185fad5af31e4a6
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2020-10-12 19:23:37 -0500

    Added firing and health
    Added the ability to shoot, deal damage and destroy targets.

---
+++ Assignment 5/3D First Person Controller/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycast.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Benjamin Schuster
+ * Assignment 5
+ * Manage firing the weapon
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycast : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hitInfo;
+         if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+         {
+            Debug.Log(hitInfo.transform.gameObject.name);
+
+            //get target script off object
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+            //deal damage if valid target
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+         }
+
+    }
+}
---
+++ Assignment 5/3D First Person Controller/Assets/MyFirstPersonPlayer/Scripts/Target.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Benjamin Schuster
+ * Assignment 5
+ * Health system for targets
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
