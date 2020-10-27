129079fef65acbd7dcdf96ea5ed3a48d2dd4b1e6
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2020-10-27 08:25:58 -0500

    Finished Singleton
    Finished Singleton video follow along.

--- Assignment 6/Singletons/Assets/Scripts/GameManager.cs
+++ Assignment 6/Singletons/Assets/Scripts/GameManager.cs
@@ -11,7 +11,10 @@ using UnityEngine.SceneManagement;
 public class GameManager : Singleton<GameManager>
 {
     public int score;
-    private string currentLevelName = string.Empty;
+    public GameObject pauseMenu;
+    public bool isPaused = false;
+
+    private string CurrentLevelName = "MainMenu";
 
     /*#region Singleton
     public static GameManager instance;
@@ -33,6 +36,20 @@ public class GameManager : Singleton<GameManager>
 
     #endregion*/
 
+    private void Update()
+    {
+        //pauses and resumes on esc press. Cannot pause on main menu screen
+        if(Input.GetKeyDown(KeyCode.Escape) && !isPaused && !CurrentLevelName.Equals("MainMenu"))
+        {
+            Pause();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused && !CurrentLevelName.Equals("MainMenu"))
+        {
+            UnPause();
+        }
+    }
+
+    //load and unload levels
     public void LoadLevel(string levelName)
     {
         AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
@@ -41,7 +58,7 @@ public class GameManager : Singleton<GameManager>
             Debug.LogError("[GameManager] Unable to load: " + levelName);
             return;
         }
-        currentLevelName = levelName;
+        CurrentLevelName = levelName;
     }
 
     public void UnLoadLevel(string levelName)
@@ -51,6 +68,31 @@ public class GameManager : Singleton<GameManager>
         {
             Debug.LogError("[GameManager] Unable to unload: " + levelName);
         }
+        CurrentLevelName = "MainMenu";
+    }
+
+    public void UnloadCurrentLevel()
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(CurrentLevelName);
+        if (ao == null)
+        {
+            Debug.LogError("[GameManager] Unable to unload: " + CurrentLevelName);
+        }
+        CurrentLevelName = "MainMenu";
+    }
+
+    //pausing and unpausing
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+    public void UnPause()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        isPaused = false;
     }
 
 }
