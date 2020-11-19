968e04d12ad889510bafc58aca5cbd52ac132716
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2020-11-19 13:28:46 -0600

    Prototype 5 Finished
    Completed prototype 5 and all requirements.

---
+++ Assignment 8/Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Benjamin Schuster
+ * Prototype 5
+ * Game manager, spawns items 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.5f;
+
+    public GameObject titleScreen;
+    public TextMeshProUGUI scoreText;
+    private int score;
+
+    public TextMeshProUGUI loseText;
+    public TextMeshProUGUI winText;
+
+    public bool isGameActive;
+    public GameObject restartButton;
+
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        titleScreen.SetActive(false);
+        scoreText.gameObject.SetActive(true);
+
+        spawnRate = 1.0f / difficulty;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            //wait 1 second
+            yield return new WaitForSeconds(spawnRate);
+
+            //Select one random object from list and spawn
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int addScore)
+    {
+        score += addScore;
+        scoreText.text = "Score: " + score;
+        if(score >= 100)
+        {
+            GameWin();
+        }
+    }
+
+    public void GameOver()
+    {
+        if(isGameActive)
+        {
+            isGameActive = false;
+            loseText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+        }
+        
+    }
+    public void GameWin()
+    {
+        if(isGameActive)
+        {
+            isGameActive = false;
+            winText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    
+
+}
---
+++ Assignment 8/Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Benjamin Schuster
+ * Prototype 5
+ * Controls the initialization of the target objects
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+    public int scoreValue;
+
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float  maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    private GameManager gameManager;
+
+    public ParticleSystem explosionParticle;
+    
+
+    void Start()
+    {
+        //initialize
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+
+        //Add force upwards multiplied by randomized seed
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+
+        //add torque with randomized xyz values
+        targetRb.AddTorque(
+            RandomTorque(),
+            RandomTorque(),
+            RandomTorque(),
+            ForceMode.Impulse);
+
+        //set the position with a randomized X value
+        transform.position = RandomSpawnPos();
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange) , ySpawnPos);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            gameManager.UpdateScore(scoreValue);
+
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+
+        Destroy(gameObject);
+
+    }
+
+
+}
